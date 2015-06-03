1d89fe203a4627dbe2929689598625babdc9af57
Rohit Nair <admin@rohitnair.net>
2015-06-03 14:49:01 +0400

    Mood recording

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def record_mood
+    current_user.moods.create(mood: params[:mood])
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root 'pages#home'
+
+  post 'record/:mood' => 'pages#record_mood', as: :record_mood
 end
